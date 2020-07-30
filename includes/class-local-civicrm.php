1d9881b2da424afd89bc1fece3cf218b3e486751
Jaap Jansma <jaap.jansma@civicoop.org>
2020-07-30 12:03:06 +0200

    fixed issue with timezone in a local civicrm installation

--- includes/class-local-civicrm.php
+++ includes/class-local-civicrm.php
@@ -18,6 +18,22 @@ class CiviCRM_Caldera_Forms_FormProcessor_LocalCiviCRM {
       return ['error' => 'CiviCRM not Initialized', 'is_error' => '1'];
     }
 
+    /*
+     * Copied from CiviCRM invoke function as there is a problem with timezones
+     * when the local connection is used.
+     *
+     * CRM-12523
+     * WordPress has it's own timezone calculations
+     * CiviCRM relies on the php default timezone which WP
+     * overrides with UTC in wp-settings.php
+     */
+    $wpBaseTimezone = date_default_timezone_get();
+    $wpUserTimezone = get_option('timezone_string');
+    if ($wpUserTimezone) {
+      date_default_timezone_set($wpUserTimezone);
+      \CRM_Core_Config::singleton()->userSystem->setMySQLTimeZone();
+    }
+
     try {
       if (!empty($options)) {
         $params['options'] = $options;
@@ -25,8 +41,16 @@ class CiviCRM_Caldera_Forms_FormProcessor_LocalCiviCRM {
       $result = civicrm_api3($entity, $action, $params);
     } catch (CiviCRM_API3_Exception $e) {
       $error = $e->getMessage() . '<br><br><pre>' . $e->getTraceAsString() . '</pre>';
-      return ['error' => $error, 'is_error' => '1'];
+      $result = ['error' => $error, 'is_error' => '1'];
     }
+
+    /*
+     * Reset the timezone back the original setting.
+     */
+    if ($wpBaseTimezone) {
+      date_default_timezone_set($wpBaseTimezone);
+    }
+
     return $result;
   }
 
